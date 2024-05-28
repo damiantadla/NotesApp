bc3bfb9ea49ea478825d4acbc1b820f9cbfae11f
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-05-28 12:34:43 +0200

    Edit feature closeUserAccount.func.js

--- functions/users/closeUserAccount.func.js
+++ functions/users/closeUserAccount.func.js
@@ -1,7 +1,20 @@
-const functions = require("firebase-functions")
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+const firestore = admin.firestore();
 
 exports.default = functions.auth.user().beforeSignIn(async (user, context) => {
-    if (user) {
-        console.log(user)
+    try {
+        const docSnapShot = await firestore.collection("users").doc(user.uid).get();
+        const data = docSnapShot.data();
+
+        if (data.block) {
+            console.log("User is blocked");
+            throw new Error('User is blocked');
+        }
+        return null;
+    } catch (error) {
+        console.log("Error fetching user record:", error);
+        throw new functions.auth.HttpsError('internal', 'Unable to fetch user record', error.message);
     }
-})
+});
