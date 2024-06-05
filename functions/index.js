b14f0cf43589cd17014c19a1980b0546b68a18c8
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-06-05 15:31:50 +0200

    Edit index.js

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const {onRequest} = require("firebase-functions/v2/https");
+
+const functions = require("firebase-functions");
 const {initializeApp} = require("firebase-admin/app");
 initializeApp();
 const {exportFunctions} = require("better-firebase-functions");
 
-const coursesRoute = require("./api/routes/courses")
-const notesRoute = require("./api/routes/notes")
-const userRoute = require("./api/routes/users")
+const coursesRoute = require("./api/routes/courses");
+const notesRoute = require("./api/routes/notes");
+const userRoute = require("./api/routes/users");
+const authRoute = require("./api/routes/auth");
 
 const app = express();
 
+app.use("/courses", coursesRoute);
+app.use("/notes", notesRoute);
+app.use("/user", userRoute);
+app.use("/auth", authRoute);
 
-app.use("/courses", coursesRoute)
-app.use("/notes", notesRoute)
-app.use("/user", userRoute)
-
-exports.api = onRequest(app);
-
+exports.api = functions.https.onRequest(app);
 
 exportFunctions({__filename, exports, searchGlob: "**/*.func.js"});
